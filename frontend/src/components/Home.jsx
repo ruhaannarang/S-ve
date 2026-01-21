288f25662072161d68e2c8e14795698984960ede
Ruhaan Narang <techieruhaan@gmail.com>
2026-01-22 02:36:14 +0530

    created homepage

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import front_page_photo from "../assets/front_page_photo.png";
+const Home = () => {
+  return (
+    
+      <body>
+      <div className="front_page_Image">
+        <img src= {front_page_photo} alt="" />
+      </div>
+      <div className="siteName">
+        <h1 id='siteName'>S+ve | Social positive</h1>
+      </div>
+      <div className='fpquote'>Being yourself is your biggest social strength.</div>
+      <div className="authButtons">
+        <a href="" className="btnauth">Sign in</a>
+        <a href="" className="btnauth">Login</a>
+      </div>
+     </body>
+    
+  )
+}
+
+export default Home
