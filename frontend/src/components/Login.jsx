60e617bfa10562fdf440b99bb29dd444e0e59772
Ruhaan Narang <techieruhaan@gmail.com>
2026-01-22 03:14:28 +0530

    loginpage ui

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>
+    <div className="loginpage">
       <form action="">
-        <h2>Login</h2>
-        
-        <input type="email" id="email" name="email" required />
-        <input type="password" id="password" name="password" required />
-        <button type="submit">Login</button>
+        <h2 className="loginpage-heading" >Login</h2>
+        <div className="logininputs">
+          <div className="logininput">
+            <label htmlFor="username">Username</label>
+            <input type="name" id="username" name="username" placeholder="username" required />
+          </div>
+          <div className="logininput">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="password" required />
+          </div>
+          <button className="loginbtn" type="submit">Login</button>
+        </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
