7e2d2f8d8c7ff7c4424bb0b53ab62dde5c5f8348
Ruhaan Narang <techieruhaan@gmail.com>
2026-01-23 00:29:00 +0530

    created login endpoint

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -9,6 +9,9 @@ dotenv.config();
 const User = require("../models/User");
 // app.use(cors());
 const JWT_SECRET = process.env.JWT_SECRET;
+
+//creating user
+
 router.post("/createuser", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) {
@@ -31,6 +34,45 @@ router.post("/createuser", async (req, res) => {
   const authtoken = jwt.sign(data, JWT_SECRET);
   res.json({ success: true, authtoken });
 });
+
+//login user
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  let user = await User.findOne({ username: username });
+  if (!user) {
+    return res
+      .status(400)
+      .json({ error: "Sorry a user with this username does not exist" });
+  }
+  const isPasswordMatch = await bcrypt.compare(password, user.password);
+  if (!isPasswordMatch) {
+    return res
+      .status(400)
+      .json({ error: "Sorry a user with this username and password does not exist" });
+  }
+  const data = {
+    user: {
+      id: user.id,
+    },
+  };
+  const authtoken = jwt.sign(data, JWT_SECRET);
+  res.json({ success: true, authtoken });
+});
+
+//getuser details
+
+router.post("/getuser", async (req, res) => {
+  const { username } = req.body;
+  let user = await User.findOne({ username: username }).select("-password");
+  if (!user) {
+    return res
+      .status(400)
+      .json({ error: "Sorry a user with this username does not exist" });
+  }
+  res.json({ success: true, user });
+});
+
 router.get("/", (req, res) => {
   res.send();
 });
